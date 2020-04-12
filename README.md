0b13329d0f279e6e0c9caee77c54cca7bada8525
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-04-12 19:19:47 +0200

    feat(services): add services endpoint
    * reuse same mongo connection
    * rename /status to /health

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # hort/mongorest
 
-Rest API to query MongoDB index from client.
+~~Rest API to query MongoDB index from client.~~
+
+One day this repository will be responsible for the interaction between the
+client and the daemon.
 
 ## Usage
 
@@ -10,13 +13,35 @@ npm start
 
 # Dependencies
 
+- cors
 - body-parser
 - express
+- js-yaml
 - mongodb
 
 # Documentation
 
-## `GET /`
+## `GET /health`
+
+Returns the status of the service
+
+```
+$ curl "http://localhost:4000/health"
+
+ok
+```
+
+## `GET /services`
+
+Returns a list of all services and their metadata
+
+```
+$ curl "http://localhost:4000/services"
+
+{"services":[...]}
+```
+
+## `GET /mongo`
 
 - `collection`: collection in which query
 - `query`: MongoDB query
@@ -27,11 +52,12 @@ $ curl "http://localhost:4000?collection=index&query=\{\}"
 [{"_id":"5e1b56a2eb920ca66368fd74","{\"last_check\":\"1578850048\"}":""}]%
 ```
 
-## `POST /`
+## `POST /mongo`
 
 - `collection`: collection where to insert documents
 
 ```
 $ curl -XPOST "http://localhost:4000?collection=index" --data '{"last_check":"1578850048"}'
-```
 
+{"ok":1,"n":1}
+```
--- server.js
+++ server.js
@@ -1,57 +1,92 @@
-var MongoClient =  require('mongodb').MongoClient;
-var url         = "mongodb://localhost:27017/";
+// Configuration
+const fs         = require('fs');
+const yaml       = require('js-yaml');
 
-var express    = require('express');
-var bodyParser = require('body-parser');
-var port       = process.env.PORT || 4000;
-var app        = express();
+// MongoDB
+const mongodb    = require('mongodb');
+const client     = mongodb.MongoClient;
+const url        = 'mongodb://localhost:27017/';
 
-app.listen(port);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Web Server
+const bodyParser = require('body-parser');
+const cors       = require('cors')
+const express    = require('express');
+
+const config     = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+const port       = process.env.PORT || config.port;
+const app        = express();
+
+client.connect(url, { useUnifiedTopology: true }, function(err, db) {
+  if (err) {
+    console.log(err);
+  } else {
+    dbo = db.db(config.db);
+  }
+});
 
 function find(req, res) {
-	if (!req.query["collection"] || !req.query["query"]) {
-		res.end();
-		return;
-	}
+  if (!req.query['collection'] || !req.query['query']) {
+    res.send('incomplete request');
+    return;
+  }
 
-	MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
-		if (err) res.send(err);
-		var dbo = db.db("hortdb");
-		dbo.collection(req.query["collection"]).find(JSON.parse(req.query["query"])).toArray(function(err, result) {
-			if (err)
-				res.send(err);
-			else
-				res.json(result);
-			db.close();
-		});
-	});
+  dbo.collection(req.query['collection'])
+     .find(JSON.parse(req.query['query']))
+     .toArray(function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
 }
 
 function insert(req, res) {
-	if (!req.query["collection"] || !req.body) {
-		res.end();
-		return;
-	}
+  if (!req.query['collection'] || !req.body) {
+    res.send('incomplete request');
+    return;
+  }
 
-	MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
-		if (err) res.send(err);
-		var dbo = db.db("hortdb");
-		if (!Array.isArray(req.body))
-			resource = [req.body];
-		else
-			resource = req.body;
-		dbo.collection(req.query["collection"]).insertMany(resource, function(err, result) {
-			if (err)
-				res.send(err);
-			else
-				res.json(result);
-			db.close();
-		});
-	});
+  if (!Array.isArray(req.body)) {
+    resource = [req.body];
+  } else {
+    resource = req.body;
+  }
+
+  dbo.collection(req.query['collection'])
+     .insertMany(resource, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result['result']);
+    }
+  });
 }
 
-app.route('')
-	.get(find)
-	.post(insert);
+function services(_, res) {
+  try {
+    res.json(config.services);
+  } catch (err) {
+    res.send('error reading the configuration file');
+  }
+}
+
+function health(_, res) {
+  res.send('ok');
+}
+
+app.route('/mongo')
+  .get(find)
+  .post(insert);
+
+app.route('/health')
+  .get(health)
+
+app.route('/services')
+  .get(services)
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port);
